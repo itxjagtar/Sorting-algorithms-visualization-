dbe41191e145b9a6ccb4292e479891e1582eb9c7
Muhammad Bilal <muhbil171845@gmail.com>
2024-04-15 10:16:14 +0500

    Visualizing each step

--- bilal.cpp
+++ bilal.cpp
@@ -1,67 +1,70 @@
-
 #include <iostream>
 using namespace std;
+
 class Selection_Sort{
-    private:
+private:
     int * arr;
     int n;
-    public:
+public:
     Selection_Sort(int n1){
         n=n1;
-       arr=new int[n];
+        arr=new int[n];
     }
     ~Selection_Sort() {
         delete[] arr;
     }
     void enterArray(){
-         for( int i=0; i<n; i++){
-        cout << "Enter the element " << i+1 << " in an array: ";
-        cin >> arr[i];
+        for( int i=0; i<n; i++){
+            cout << "Enter the element " << i+1 << " in the array: ";
+            cin >> arr[i];
+        }
     }
-    }
+
     void initialArray(){
-        cout << "Initial array: " << endl;
-    for( int i=0; i<n; i++){
-       cout << arr[i] << " ";
+        cout << "Initial array: ";
+        for( int i=0; i<n; i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
-     cout << endl;
-    }
+
     void sortArray(){
         int min;
-    for(int i=0; i<n-1; i++){
-      min=i;
-      for(int j=i+1; j<n; j++){
-      if(arr[j] < arr[min]){
-        min=j;
-      }
-      }
-      if(min !=i){
-        int temp;
-        temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp;
-      }
+        for(int i=0; i<n-1; i++){
+            min=i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[min]){
+                    min=j;
+                }
+            }
+            if(min !=i){
+                int temp;
+                temp=arr[i];
+                arr[i]=arr[min];
+                arr[min]=temp;
+            }
+            cout << "Array after iteration " << i+1 << ": ";
+            for(int k=0; k<n; k++){
+                cout << arr[k] << " ";
+            }
+            cout << endl;
+        }
     }
-    }
+
     void displaySortedArray(){
-        cout << "Sorted Array: " << endl;
-         for(int k=0; k<n; k++){
-        cout << arr[k] << " ";
-    }
+        cout << "Sorted Array: ";
+        for(int k=0; k<n; k++){
+            cout << arr[k] << " ";
+        }
+        cout << endl;
     }
 };
+
 int main(){
     int n;
-    cout << "Enter the number of elements in array: ";
+    cout << "Enter the number of elements in the array: ";
     cin >> n;
-     if (n <= 0) {
+    if (n <= 0) {
         cout << "Invalid input. Number of elements must be positive." << endl;
         return 1;
     }
-    Selection_Sort s1(n);
-    s1.enterArray();
-    s1.initialArray();
-    s1.sortArray();
-    s1.displaySortedArray();
-    return 0;
-}
