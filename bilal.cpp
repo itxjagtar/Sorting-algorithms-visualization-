7f54a3c3ad59dcb1d4a7b8c4b3f7fe0eecb18eac
Muhammad Bilal <muhbil171845@gmail.com>
2024-05-02 18:52:52 +0500

    Implement heap object in main function

--- bilal.cpp
+++ bilal.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class HeapSort {
 private:
     int *heap;
@@ -21,6 +22,7 @@ private:
             heapify(n, largest);
         }
     }
+
 public:
     HeapSort(int arr[], int n) {
         heap = arr;
@@ -36,91 +38,106 @@ public:
             heapify(i, 0);
         }
     }
- void printArray() {
+
+    void printArray() {
         for (int i = 0; i < size; ++i)
             cout << heap[i] << " ";
         cout << "\n";
     }
 };
 
-class Selection_Sort{
+class SelectionSort {
 private:
-    int * arr;
+    int *arr;
     int n;
+
 public:
-    Selection_Sort(int n1){
-        n=n1;
-        arr=new int[n];
+    SelectionSort(int n1) {
+        n = n1;
+        arr = new int[n];
     }
-    ~Selection_Sort() {
+
+    ~SelectionSort() {
         delete[] arr;
     }
-    void enterArray(){
-        for( int i=0; i<n; i++){
-            cout << "Enter the element " << i+1 << " in the array: ";
+
+    void enterArray() {
+        for (int i = 0; i < n; i++) {
+            cout << "Enter the element " << i + 1 << " in the array: ";
             cin >> arr[i];
         }
     }
 
-   void initialArray(){
+    void initialArray() {
         cout << "Initial array: ";
-        for( int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             cout << "|" << arr[i] << "| ";
         }
         cout << endl;
     }
 
-    void sortArray(){
+    void sortArray() {
         int min;
-        for(int i=0; i<n-1; i++){
-            min=i;
-            for(int j=i+1; j<n; j++){
-                if(arr[j] < arr[min]){
-                    min=j;
+        for (int i = 0; i < n - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
             }
-            if(min !=i){
+            if (min != i) {
                 int temp;
-                temp=arr[i];
-                arr[i]=arr[min];
-                arr[min]=temp;
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
             }
             cout << "Press Enter to see the next iteration...";
             cin.get();
-            cout << "Array after iteration " << i+1 << ":" << endl;
-           for(int k = 0; k < n; k++) {
-            if(k == i || k == min) {
+            cout << "Array after iteration " << i + 1 << ":" << endl;
+            for (int k = 0; k < n; k++) {
+                if (k == i || k == min) {
                     cout << "|*" << arr[k] << "*| ";
-                }else{
-            cout << "| " << arr[k] << " ";
-        }}
-        cout << "|" << endl;
+                } else {
+                    cout << "| " << arr[k] << " ";
+                }
+            }
+            cout << "|" << endl;
         }
     }
 
- void displaySortedArray(){
+    void displaySortedArray() {
         cout << "Sorted Array: ";
-        for(int k=0; k<n; k++){
+        for (int k = 0; k < n; k++) {
             cout << "|" << arr[k] << "| ";
         }
         cout << endl;
     }
 };
 
-int main(){
+int main() {
     int n;
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
     cout << "Enter the number of elements in the array: ";
     cin >> n;
     if (n <= 0) {
         cout << "Invalid input. Number of elements must be positive." << endl;
         return 1;
     }
-    Selection_Sort s1(n);
+    SelectionSort s1(n);
     s1.enterArray();
     s1.initialArray();
-    cout << "Press Enter to start sorting..." << endl;
+    cout << "Press Enter to start selection sort..." << endl;
     cin.ignore();
     s1.sortArray();
     s1.displaySortedArray();
+    HeapSort h1(arr, arr_size);
+    cout << "Original array: ";
+    for (int i = 0; i < arr_size; ++i)
+        cout << arr[i] << " ";
+    h1.sort();
+    cout << "Sorted array using heap sort: ";
+    h1.printArray();
+
     return 0;
 }
